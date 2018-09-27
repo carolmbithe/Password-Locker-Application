caba3a43dbcc05bc7bdca629b7dd5ee8fb14d628
carolmbithe <carombithe97@gmail.com>
2018-09-27 15:27:45 +0300

    created a credential class

---
+++ credential.py
@@ -0,0 +1,14 @@
+class Credential:
+    """
+    Class that generates new instances of Credential
+    """
+    def __init__(self,account_type,user_name,password):
+        """
+        __init__ method that helps us define properties of our objects
+        Args:
+        account_type:The type of account_type
+        user_name:The username for that account .
+        password:password for that account.
+
+        self variable represents the instance of the object
+        """
